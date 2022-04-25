29c90fd99884bc851176f68732ccd766f44c7069
Dany Corleone <corleonedany@live.fr>
2022-04-25 21:51:22 +0200

    include router urls

--- OPC_12/OPC_12/urls.py
+++ OPC_12/OPC_12/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
@@ -25,4 +26,5 @@ router.register('events', EventViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
