65194f68dc91197740139233daa52a3b9a62fff6
Dany Corleone <corleonedany@live.fr>
2022-04-27 02:33:43 +0200

    correction syntaxe date_joined

--- OPC_12/EpicEvents/serializers.py
+++ OPC_12/EpicEvents/serializers.py
@@ -28,4 +28,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-        read_only_fields = ('id', 'DateJoined')
+        read_only_fields = ('id', 'date_joined')
