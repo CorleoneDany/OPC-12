c3b779bea4932d2c59994f28c43852bd409b9190
Dany Corleone <corleonedany@live.fr>
2022-05-04 02:03:07 +0200

    get_queryset changed for all viewsets, errors fixed

--- OPC_12/EpicEvents/views.py
+++ OPC_12/EpicEvents/views.py
@@ -1,4 +1,3 @@
-from urllib import response
 from .models import Event, Client, Contract, ContractStatus
 from django.contrib.auth.models import User
 from .serializers import EventSerializer, ClientSerializer, ContractSerializer, UserSerializer
@@ -7,7 +6,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.response import Response
-
 
 # Create your views here.
 
@@ -18,7 +16,7 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (HasUserPermission, IsAuthenticated)
 
     def destroy(self, request, *args, **kwargs):
-        return Response(status=401)
+        return Response(status=401, data={'detail': 'You cannot delete users.'})
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -28,17 +26,29 @@ class EventViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['Client__LastName', 'Client__Email', 'EventDate']
 
+    def get_queryset(self):
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            return Event.objects.all()
+        else:
+            return Event.objects.filter(SupportContact=self.request.user)
+
     def create(self, request, *args, **kwargs):
+        used_contracts_ids = Event.objects.all().values_list('EventStatus__id', flat=True)
         serializer = EventSerializer(data=request.data)
         if serializer.is_valid():
-            contract_status = ContractStatus.objects.create(
-                Status=request.data['Status'])
-            serializer.save(SupportContact=request.user,
-                            EventStatus=contract_status)
-            return Response(serializer.data)
+            chosen_contract = ContractStatus.objects.get(
+                pk=request.data['EventStatus'])
+            if chosen_contract.id in used_contracts_ids:
+                return Response(status=400, data={'detail': 'This contract already has an event.'})
+            if chosen_contract.Signed == True:
+                serializer.save(SupportContact=request.user)
+                return Response(serializer.data)
+            else:
+                return Response(status=400, data={'Error': 'EventStatus must be True.'})
+        return Response(status=400, data=serializer.errors)
 
     def destroy(self, request, *args, **kwargs):
-        return Response(status=401)
+        return Response(status=401, data={'detail': 'You cannot delete events.'})
 
 
 class ClientViewSet(viewsets.ModelViewSet):
@@ -48,6 +58,17 @@ class ClientViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['LastName', 'Email']
 
+    def get_queryset(self):
+        if self.request.user.groups.filter(name='Support'):
+            assigned_clients = Event.objects.filter(
+                SupportContact=self.request.user).values_list('Client__id', flat=True)
+            if assigned_clients:
+                return Client.objects.filter(id__in=assigned_clients)
+        elif self.request.user.is_staff or self.request.user.is_superuser:
+            return Client.objects.all()
+        else:
+            return Client.objects.filter(SalesContact=self.request.user)
+
     def create(self, request, *args, **kwargs):
         serializer = ClientSerializer(data=request.data)
         if serializer.is_valid():
@@ -55,7 +76,7 @@ class ClientViewSet(viewsets.ModelViewSet):
             return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
-        return Response(status=401)
+        return Response(status=401, data={'detail': 'You cannot delete clients.'})
 
 
 class ContractViewSet(viewsets.ModelViewSet):
@@ -64,14 +85,44 @@ class ContractViewSet(viewsets.ModelViewSet):
     permission_classes = (HasContractPermission, IsAuthenticated)
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['Client__LastName',
-                        'Client__Email', 'DateCreated', 'Amount']
+                        'Client__Email', 'DateCreated',
+                        'Amount']
+
+    def get_queryset(self):
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            return Contract.objects.all()
+        else:
+            return Contract.objects.filter(SalesContact=self.request.user)
 
     def create(self, request, *args, **kwargs):
         serializer = ContractSerializer(data=request.data)
+        try:
+            Signed = request.data['Status'] == 'true'
+        except KeyError:
+            Signed = False
         if serializer.is_valid():
             serializer.save(SalesContact=request.user)
+            contract_status = ContractStatus.objects.create(Signed=Signed)
+            contract_status.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
 
+    def update(self, request, pk, *args, **kwargs):
+        contract = Contract.objects.get(pk=pk)
+        contract_status = ContractStatus.objects.get(pk=pk)
+        serializer = ContractSerializer(contract, data=request.data)
+
+        try:
+            Signed = request.data['Status'] == 'true'
+        except KeyError:
+            Signed = False
+        if serializer.is_valid():
+            serializer.save()
+            contract_status.Signed = Signed
+            contract_status.save()
+            return Response(serializer.data)
+        else:
+            return Response(status=400, data={'Error': 'Status must be True.'})
+
     def destroy(self, request, *args, **kwargs):
-        return Response(status=401)
+        return Response(status=401, data={'detail': 'You cannot delete contracts.'})
