aa7d6c97fd1cf2d74306fa0878af0dff1d5e527a
Dany Corleone <corleonedany@live.fr>
2022-05-04 06:57:49 +0200

    update v-env

--- OPC_12/v-env.sh
+++ OPC_12/v-env.sh
@@ -1,7 +1,9 @@
+echo $OSTYPE
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     python3 -m venv .venv
     . .venv/Scripts/activate
-    pip install -r requirements.txt
+    pip3 install -r requirements.txt
 
 elif [[ "$OSTYPE" == "msys" ]]; then
     python -m venv .venv
@@ -11,3 +13,5 @@ elif [[ "$OSTYPE" == "msys" ]]; then
 else 
     echo "Incompatible OS"
 fi
+
+read  -n 1 -p "Press enter to close this program." mainmenuinput
